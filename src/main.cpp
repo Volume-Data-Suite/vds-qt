bbe12710f66f746241d0bc6b74f5dd1fccb368c5
Frederic <frederic.laing.development@gmail.com>
2019-05-16 18:19:35 +0200

    Added working files

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+
+//#include "../external/Volume-Data-Toolkit/src/VolumeDataHandler.h"
+//#include <vdtk_lib/VolumeDataHandler.h>
+//#include <VolumeDataHandler.h>
+
+#include <iostream>
+
+#include <VDTK/VolumeDataHandler.h>
+
+
+
+//
+int main() {
+	VDTK::VolumeDataHandler vdh;
+
+	system("pause");
+
+	return 0;
+}
